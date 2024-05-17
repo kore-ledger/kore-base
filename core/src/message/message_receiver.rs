e0d36c2c92c05d2d3ef83e2d1c5d1462f250f558
Alejandro Lugo <alu0101329185@ull.edu.es>
2024-05-17 12:59:15 +0100

    format

--- core/src/message/message_receiver.rs
+++ core/src/message/message_receiver.rs
@@ -42,57 +42,57 @@ impl<T: TaskCommandContent + Serialize + DeserializeOwned + 'static> MessageRece
     pub async fn run(mut self) {
         loop {
             tokio::select! {
-                event = self.receiver.next() =>if let Some(event) = event {
-                                         match event {
-                                            NetworkEvent::MessageReceived {message, ..} => {
-                                                // The message will be a string for now
-                                                 // Deserialize the message
-                    let cur = Cursor::new(message);
-                                                let mut de = Deserializer::new(cur);
-                                                 let message: Signed<MessageContent<T>> = Deserialize::deserialize(&mut de).expect("Fallo de deserialización");
-                                                 // Check message signature
-                                                 if message.verify().is_err() || message.content.sender_id != message.signature.signer {
-                                                     log::error!("Invalid signature in message");
-                                                } else if message.content.receiver != self.own_id {
-                                                     log::error!("Message not for me");
-                                                 } else {
-                                                    self.sender.tell(message).await.expect("Channel Error");
-                                                 }
-                                            },
-                                            NetworkEvent::ConnectedToBootstrap { .. } => {
-                                             println!("NETWORK EVENT CONNECTEDTOBOOTSTRAP");
-                                            },
-                                            NetworkEvent::MessageSent { .. } => {
-                                             println!("NETWORK EVENT MESSAGESENT");
-                                            },
-                                            NetworkEvent::PeerIdentified { .. } => {
-                                             println!("NETWORK EVENT PEERIDENTIFIED");
-                                            },
-                                            NetworkEvent::StateChanged(state) => {
-                                             match state {
-                                                 NetworkState::Start=> {
-                                                     println!("NETWORK EVENT STATECHANGED START");
-                                                 },
-                                                 NetworkState::Dial => {
-                                                     println!("NETWORK EVENT STATECHANGED DIAL");
-                                                 },
-                                                 NetworkState::Dialing => {
-                                                    println!("NETWORK EVENT STATECHANGED DIALING");
-                                                 },
-                                                 NetworkState::Running => {
-                                                     println!("NETWORK EVENT STATECHANGED RUNNING");
-                                                 },
-                                                 NetworkState::Disconnected => {
-                                                     println!("NETWORK EVENT STATECHANGED DISCONNECTED");
-                                                     break;
-                                                 }
-                                             }
-                                            },
-                                            NetworkEvent::Error(_) => {
-                                             println!("NETWORK EVENT ERROR");
-                                            },
-                                         }
-                                     },
+                event = self.receiver.next() => if let Some(event) = event {
+                        match event {
+                           NetworkEvent::MessageReceived {message, ..} => {
+                                // The message will be a string for now
+                                // Deserialize the message
+                                let cur = Cursor::new(message);
+                                let mut de = Deserializer::new(cur);
+                                let message: Signed<MessageContent<T>> = Deserialize::deserialize(&mut de).expect("Fallo de deserializaciÃ³n");
+                                // Check message signature
+                                if message.verify().is_err() || message.content.sender_id != message.signature.signer {
+                                    log::error!("Invalid signature in message");
+                                } else if message.content.receiver != self.own_id {
+                                    log::error!("Message not for me");
+                                } else {
+                                    self.sender.tell(message).await.expect("Channel Error");
+                                }
+                            },
+                            NetworkEvent::ConnectedToBootstrap { .. } => {
+                                println!("NETWORK EVENT CONNECTEDTOBOOTSTRAP");
+                            },
+                            NetworkEvent::MessageSent { .. } => {
+                                println!("NETWORK EVENT MESSAGESENT");
+                            },
+                            NetworkEvent::PeerIdentified { .. } => {
+                                println!("NETWORK EVENT PEERIDENTIFIED");
+                            },
+                            NetworkEvent::StateChanged(state) => {
+                            match state {
+                                NetworkState::Start=> {
+                                    println!("NETWORK EVENT STATECHANGED START");
+                                },
+                                NetworkState::Dial => {
+                                    println!("NETWORK EVENT STATECHANGED DIAL");
+                                },
+                                NetworkState::Dialing => {
+                                    println!("NETWORK EVENT STATECHANGED DIALING");
+                                },
+                                NetworkState::Running => {
+                                    println!("NETWORK EVENT STATECHANGED RUNNING");
+                                },
+                                NetworkState::Disconnected => {
+                                    println!("NETWORK EVENT STATECHANGED DISCONNECTED");
+                                    break;
+                                }
+                            }
+                           },
+                           NetworkEvent::Error(_) => {
+                            println!("NETWORK EVENT ERROR");
+                           },
+                        }
+                },
                 _ = self.token.cancelled() => {
                     log::info!("Message receiver module shutdown received");
                     break;
